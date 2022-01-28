e7493c8c3645f87f801e9709678d009b494e0814
Adan Abbi <adanweli@hotmail.com>
2022-01-28 00:14:24 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React because its a great tool
+          Learn React because its a great tool, ever!
         </a>
       </header>
     </div>
